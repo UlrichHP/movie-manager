335191549b7593f2ac1b85fd53f534d23b885707
Ulrich <ulrichhuet@hotmail.com>
2023-09-29 11:26:56 +0200

    feat(movie): added db factory and seeder

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use function fake;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
+ */
+class MovieFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->text,
+            'year' => $this->faker->year,
+            'duration' => $this->faker->time,
+        ];
+    }
+}
