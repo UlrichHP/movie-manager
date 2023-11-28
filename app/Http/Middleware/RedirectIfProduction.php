a36a29d7a7fefd38267e0f1c6dbeefc1eeb6cec6
Ulrich <ulrichhuet@hotmail.com>
2023-11-28 18:59:38 +0100

    feat(swagger): added RedirectIfProduction middleware
    to prevent access to api/documentation in prod

---
+++ app/Http/Middleware/RedirectIfProduction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Providers\RouteServiceProvider;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+use function redirect;
+
+class RedirectIfProduction
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (app()->environment() !== 'local') {
+            return redirect(RouteServiceProvider::HOME);
+        }
+
+        return $next($request);
+    }
+}
