1bdc8107c44f6f813fa01e26d7f1ecbb0e05ce92
Ulrich <ulrichuet@gmail.com>
2023-12-02 17:09:49 +0100

    feat(movie): added actors and genres to show page

--- resources/js/components/movie/Movie.vue
+++ resources/js/components/movie/Movie.vue
@@ -11,15 +11,28 @@
         <div class="container">
             <p><strong>Année :</strong> {{ movie.year }}</p>
             <p><strong>Durée :</strong> {{ movie.duration }}</p>
+            <p><strong>Genre(s) : </strong>
+                <template v-for="(genre, index) in movie.genres">
+                    {{ genre.name }}
+                    <span v-if="index !== movie.genres.length - 1">, </span>
+                </template>
+            </p>
+            <p><strong>Acteur(s) : </strong>
+                <template v-for="(genre, index) in movie.actors">
+                    {{ genre.name }}
+                    <span v-if="index !== movie.actors.length - 1">, </span>
+                </template>
+            </p>
             <p>{{ movie.description }}</p>
         </div>
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
     import { ref } from 'vue'
+    import { MovieType } from '../../types/MovieType';
 
-    const movie = ref([])
+    const movie = ref<MovieType|[]>([])
 
     const url = window.location.href
     const id = url.split('/').slice(-1)[0]
--- resources/js/types/MovieType.ts
+++ resources/js/types/MovieType.ts
@@ -1,7 +1,12 @@
+import { ActorType } from './ActorType'
+import { GenreType } from './GenreType'
+
 export type MovieType = {
     id: number,
     title: string,
     description: string,
     year: number,
     duration: string,
+    actors: Array<ActorType>,
+    genres: Array<GenreType>,
 }
