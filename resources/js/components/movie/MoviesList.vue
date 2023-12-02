049483d7de9262acc79f5dac4341f32f4df21048
Ulrich <ulrichuet@gmail.com>
2023-12-02 17:28:12 +0100

    feat(movie): added basic search by title

--- resources/js/components/movie/MoviesList.vue
+++ resources/js/components/movie/MoviesList.vue
@@ -1,5 +1,11 @@
 <template>
     <div class="container">
+        <section class="mb-3">
+            <div class="form-check form-check-inline">
+                <input v-model.trim="searchInput"  @keyup="getResults()" class="form-control" type="text" placeholder="Votre recherche">
+            </div>
+        </section>
+
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3 mb-3">
             <div class="col" v-for="movie in movies.data" :key="movie.id">
                 <MovieCard :movie="movie" />
@@ -20,13 +26,20 @@
     import MovieCard from './MovieCard.vue';
 
     const movies = ref([])
+    const searchInput = ref('')
 
     const getResults = async (page = 1) => {
+        const params = {
+            page,
+        }
+
+        if (searchInput.value.length > 0) {
+            params.title = searchInput.value
+        }
+
         await axios
             .get('/api/movies', {
-                params: {
-                    page,
-                }
+                params
             })
             .then((res) => movies.value = res.data.data)
             .catch((err) => console.log(err))
